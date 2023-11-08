3f5f595dfec176c6411f2c10931df6d07979b27b
Shenghan Yang <211250179@smail.nju.edu.cn>
2023-11-08 16:21:46 +0800

    branch: add security issue

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
-import { createApp, VueElement } from 'vue'
-import App from './App.vue'
-import Router from './router/index.js'
+import { createApp, VueElement } from "vue";
+import App from "./App.vue";
+import Router from "./router/index.js";
 
-import 'element-plus/theme-chalk/el-loading.css';
-import 'element-plus/theme-chalk/el-message-box.css';
-import 'element-plus/theme-chalk/el-message.css';
+import "element-plus/theme-chalk/el-loading.css";
+import "element-plus/theme-chalk/el-message-box.css";
+import "element-plus/theme-chalk/el-message.css";
 
-import 'element-plus/theme-chalk/dark/css-vars.css'
+import "element-plus/theme-chalk/dark/css-vars.css";
 
-import storage from './utils/LocalStorage.js'
+import storage from "./utils/LocalStorage.js";
 
-import './assets/main.css'
+import "./assets/main.css";
 import axios from "axios";
 
-VueElement.prototype.$storage=storage;
+VueElement.prototype.$storage = storage;
 
-const app = createApp(App)
-app.use(Router)
-app.mount('#app')
+const app = createApp(App);
+app.use(Router);
+app.mount("#app");
 
-function checkForSecurityIssue(){
-    console.log(undeclaredVariable);
+function checkForSecurityIssue() {
+  console.log(undeclaredVariable);
+  let userContent = document.getElementById("userContent").value;
+  document.getElementById("display").innerHTML = userContent;
 }
