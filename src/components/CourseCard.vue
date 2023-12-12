d606e60635713fb4453c3002b28d31b2a4717aea
liukedian <91513633+liukedian@users.noreply.github.com>
2023-12-12 16:34:32 +0800

    完成部分任务

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -1,107 +1,151 @@
 <script setup>
-import { useRouter } from 'vue-router';
-import CourseTag from './CourseTag.vue';
+import { useRouter } from "vue-router";
+import CourseTag from "./CourseTag.vue";
 
 const props = defineProps({
-    // 展示课程信息列表
-    courses: {
-        type: Array,
-        default: [],
-    },
-})
+  // 展示课程信息列表
+  courses: {
+    type: Array,
+    default: [],
+  },
+});
 
-const router = useRouter()
+const router = useRouter();
 
 // 跳转课程详情页
 function goToCourse(item) {
-    router.push({ path: '/detail', query: { courses: item.coursename, itro: item.introduction, teacher: item.teachername, during: item.studytime, picture: item.picture, id: item.id , viewtime: item.viewtimes} })
+  router.push({
+    path: "/detail",
+    query: {
+      courses: item.coursename,
+      itro: item.introduction,
+      teacher: item.teachername,
+      during: item.studytime,
+      picture: item.picture,
+      id: item.id,
+      viewtime: item.viewtimes,
+    },
+  });
 }
-
 </script>
 
 <template>
-    <el-card v-for="item in courses" :key="item.courseid" class="course-card" style="--el-card-padding: 0">
-        <template #header>
-            <img :src="'data:image/png;base64,' + item.picture" @click="goToCourse(item)" />
-            <div class="card-header">
-                <div class="card-header-name" @click="goToCourse(item)">{{ item.coursename }}</div>
-                <div class="card-header-tags">
-                    <!-- <CourseTag style="margin: 0 0.5rem 0 0;" tag="课程" color="rgb(126, 125, 187)" /> -->
-                    <CourseTag style="margin: 0 0.5rem 0 0;" :tag="item.department" color="rgb(119, 127, 79)" />
-                    <CourseTag style="margin: 0 0.5rem 0 0;" :tag="item.studytime" color="rgb(79, 49, 45)" />
-                </div>
-            </div>
-        </template>
-        <p class="card-intro">
-            {{ item.introduction}}
-        </p>
-    </el-card>
+  <el-card
+    v-for="item in courses"
+    :key="item.courseid"
+    class="course-card"
+    style="--el-card-padding: 0"
+  >
+    <template #header>
+      <img
+        :src="'data:image/png;base64,' + item.picture"
+        @click="goToCourse(item)"
+      />
+      <div class="card-header">
+        <div class="card-header-name" @click="goToCourse(item)">
+          {{ item.coursename }}
+          <img
+            src="src\resources\shoucang.png"
+            style="float: right"
+            id="shoucang"
+          />
+        </div>
+
+        <div class="card-header-tags">
+          <!-- <CourseTag style="margin: 0 0.5rem 0 0;" tag="课程" color="rgb(126, 125, 187)" /> -->
+          <CourseTag
+            style="margin: 0 0.5rem 0 0"
+            :tag="item.department"
+            color="rgb(119, 127, 79)"
+          />
+          <CourseTag
+            style="margin: 0 0.5rem 0 0"
+            :tag="item.studytime"
+            color="rgb(79, 49, 45)"
+          />
+        </div>
+      </div>
+    </template>
+
+    <p class="card-intro" @click="goToCourse(item)" id="card-int">
+      {{ item.introduction }}
+    </p>
+  </el-card>
 </template>
 
 <style scoped>
 @keyframes PopUp {
-    from {
-        scale: 0.6;
-    }
+  from {
+    scale: 0.6;
+  }
 
-    to {
-        scale: 1;
-    }
+  to {
+    scale: 1;
+  }
 }
 
+#shoucang {
+  width: 1.5rem;
+  height: 1.5rem;
+  float: True;
+  margin-top: 5px;
+}
 .course-card {
-    width: 14rem;
-    box-shadow: 0 1px 3px hsl(0deg 0% 7% / 10%);
-    border-radius: 8px;
-    margin: 0 1rem 2rem 1rem;
-    box-sizing: border-box;
-    transition: all 0.3s;
-    animation-name: PopUp;
-    animation-duration: 0.5s;
+  width: 14rem;
+  box-shadow: 0 1px 3px hsl(0deg 0% 7% / 10%);
+  border-radius: 8px;
+  margin: 0 1rem 2rem 1rem;
+  box-sizing: border-box;
+  transition: all 0.3s;
+  animation-name: PopUp;
+  animation-duration: 0.5s;
 }
 
 .course-card:hover {
-    box-shadow: var(--el-box-shadow);
+  box-shadow: var(--el-box-shadow);
 }
 
 .card-header {
-    font-size: large;
-    font-weight: bolder;
-    padding: 0.5rem 1rem;
+  font-size: large;
+  font-weight: bolder;
+  padding: 0.5rem 1rem;
 }
 
 .card-header-name {
-    font-weight: bolder;
-    cursor: pointer;
-    transition: all 0.5s;
+  font-weight: bolder;
+  cursor: pointer;
+  transition: all 0.5s;
 }
 
 .card-header-name:hover {
-    color: var(--color-main);
+  color: var(--color-main);
 }
 
 .card-header-tags {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin-top: 6px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 6px;
 }
 
 .card-intro {
-    padding: 0.5rem 1rem 1rem 1rem;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
+  padding: 0.5rem 1rem 1rem 1rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 
+#card-int:hover {
+  box-shadow: var(--el-box-shadow);
+  color: var(--color-main);
+}
 img {
-    transition: all 0.5s;
-    cursor: pointer;
-    width: 100%;
+  transition: all 0.5s;
+  cursor: pointer;
+  width: 100%;
 }
 
 img:hover {
-    transform: scale(1.1);
+  transform: scale(1.1);
 }
-
 </style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,47 +1,87 @@
 <script setup>
-
-import { ref, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
-import { Search } from '@element-plus/icons-vue'
-
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { Search } from "@element-plus/icons-vue";
+import { ElLoading } from "element-plus";
 // 搜索内容
-const content = ref('')
+const content = ref("");
 
-const router = useRouter()
+const router = useRouter();
 
 function goToSearch() {
+  const loading = ElLoading.service({
+    text: "Loading",
+  });
+  setTimeout(() => {
+    loading.close();
     if (content.value.length > 0) {
-        router.push({ path: '/search', query: { content: content.value } })
+      router.push({ path: "/search", query: { content: content.value } });
     } else {
-        router.push({path: '/'})
+      router.push({ path: "/" });
     }
+  }, 300);
 }
 
-const inputWidth = ref('6rem')
+const inputWidth = ref("6rem");
 function focus() {
-    inputWidth.value='var(--search-input-width)'
+  inputWidth.value = "var(--search-input-width)";
 }
 function blur() {
-    inputWidth.value = '6rem'
+  inputWidth.value = "6rem";
+}
+
+function searchEnterFun(e) {
+  var keyCode = e.keyCode;
+  if (keyCode == 13) {
+    goToSearch();
+  }
 }
 </script>
 
 <template>
-
-    <div style="display: flex; flex-direction: row; width: var(--search-bar-width); justify-content: flex-end;">
-        <el-input class="search-input" @focus="focus" @blur="blur" :style="{'width': inputWidth}" v-model="content" maxlength="15" placeholder="搜索课程" size="large" clearable>
-        </el-input>
-        <el-button :icon="Search" style="margin: 0 0 0 1rem; --el-button-hover-bg-color: var(--color-main-darker); --el-button-hover-border-color: var(--color-main-darker); --el-button-active-bg-color: var(--color-main-darkest);" type="primary" color="var(--color-main)" size="large" @click="goToSearch"
-            circle></el-button>
-    </div>
-
+  <div
+    style="
+      display: flex;
+      flex-direction: row;
+      width: var(--search-bar-width);
+      justify-content: flex-end;
+    "
+  >
+    <el-input
+      class="search-input"
+      @focus="focus"
+      @blur="blur"
+      :style="{ width: inputWidth }"
+      v-model="content"
+      maxlength="15"
+      placeholder="快来搜索课程或资源吧"
+      size="large"
+      clearable
+      @keyup.enter="searchEnterFun"
+    >
+    </el-input>
+    <el-button
+      :icon="Search"
+      style="
+        margin: 0 0 0 1rem;
+        --el-button-hover-bg-color: var(--color-main-darker);
+        --el-button-hover-border-color: var(--color-main-darker);
+        --el-button-active-bg-color: var(--color-main-darkest);
+      "
+      type="primary"
+      color="var(--color-main)"
+      size="large"
+      @click="goToSearch"
+      circle
+    ></el-button>
+  </div>
 </template>
 
 <style scoped>
 .search-input {
-    --el-input-border-radius: 50px;
-    --el-input-focus-border-color: var(--color-main);
-    transition: all 0.5s;
-    --el-input-bg-color: transparent;
+  --el-input-border-radius: 50px;
+  --el-input-focus-border-color: var(--color-main);
+  transition: all 0.5s;
+  --el-input-bg-color: transparent;
 }
 </style>
